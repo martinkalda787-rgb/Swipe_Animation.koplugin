e6561e1d2b88c68fb4dd69e477359d1a033fb84d
Fallen Genius Programmer <martinkalda787@gmail.com>
2026-07-07 06:41:28 +0800

    Update 2-pdf-animation.lua

--- patches/2-pdf-animation.lua
+++ patches/2-pdf-animation.lua
@@ -7,12 +7,10 @@ local original_gotoPage = ReaderPaging._gotoPage
 function ReaderPaging:_gotoPage(number, orig_mode)
     -- Check if we are turning a page and not in scroll mode
     if self.current_page and self.current_page > 0 and number ~= self.current_page and not self.view.page_scroll then
-        -- Ensure swipe animations are globally enabled in settings
-        if not G_reader_settings:isTrue("swipe_animations") then
-            G_reader_settings:saveSetting("swipe_animations", true)
+        if G_reader_settings:isTrue("swipe_animations") then
+            local forward = number > self.current_page
+            self.ui:handleEvent(Event:new("PageChangeAnimation", forward))
         end
-        local forward = number > self.current_page
-        self.ui:handleEvent(Event:new("PageChangeAnimation", forward))
     end
     -- Call the original page turn logic
     return original_gotoPage(self, number, orig_mode)
